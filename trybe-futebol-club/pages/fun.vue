221c2bda24bdc5daa640c6f0dbd138fa1ad7636e
victrosantos <victor.santos.fk@hotmail.com>
2023-07-25 17:03:16 -0300

    feat: Iniciado projeto apos estudos do nuxtjs

---
+++ trybe-futebol-club/pages/fun.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p v-if="$fetchState.pending">Fetching mountains...</p>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <div v-else>
+      <h1>Nuxt Mountains</h1>
+      <ul>
+        <li v-for="mountain of mountains">
+          {{ mountain.title }}
+          <img :src="mountain.image" class="mountain-img" :alt="mountain.title" />
+        </li>
+      </ul>
+      <button @click="$fetch">Refresh</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        mountains: []
+      }
+    },
+    async fetch() {
+      this.mountains = await fetch(
+        'https://api.nuxtjs.dev/posts'
+      ).then(res => res.json())
+    }
+  }
+</script>
+
+<style>
+.mountain-img {
+  max-height: 450px;
+}
+</style>
